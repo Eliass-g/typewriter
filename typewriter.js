e64cdddf5732b225f7c3a1bcddab86eb13c12b5a
Eliass-g <eliass.ghauss@gmail.com>
2023-09-26 16:11:00 -0400

    added typewriter.js

---
+++ typewriter.js
@@ -0,0 +1,13 @@
+const sentence = "hello there from lighthouse labs";
+
+let n = 0;
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, n)
+  n += 50;
+}
+
+setTimeout(() => {
+  process.stdout.write("\n");
+}, n)
